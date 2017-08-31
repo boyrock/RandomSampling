90ea731d796b9229e832f643a1bc2fc847595589
JiyongLee <lee@team-lab.com>
2017-08-31 17:53:57 +0900

    first comiit

---
+++ Assets/Scripts/MeshHelper.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class MeshHelper
+{
+    static List<Vector3> vertices;
+    static List<Vector3> normals;
+    static List<Color> colors;
+    static List<Vector2> uv;
+    static List<Vector2> uv1;
+    static List<Vector2> uv2;
+
+    static List<int> indices;
+    static Dictionary<uint, int> newVectices;
+
+    static void InitArrays(Mesh mesh)
+    {
+        vertices = new List<Vector3>(mesh.vertices);
+        normals = new List<Vector3>(mesh.normals);
+        colors = new List<Color>(mesh.colors);
+        uv = new List<Vector2>(mesh.uv);
+        uv1 = new List<Vector2>(mesh.uv2);
+        uv2 = new List<Vector2>(mesh.uv3);
+        indices = new List<int>();
+        //indices = new List<int>(mesh.triangles);
+    }
+    static void CleanUp()
+    {
+        vertices = null;
+        normals = null;
+        colors = null;
+        uv = null;
+        uv1 = null;
+        uv2 = null;
+        indices = null;
+    }
+
+    #region Subdivide4 (2x2)
+    static int GetNewVertex4(int i1, int i2)
+    {
+        int newIndex = vertices.Count;
+        uint t1 = ((uint)i1 << 16) | (uint)i2;
+        uint t2 = ((uint)i2 << 16) | (uint)i1;
+        if (newVectices.ContainsKey(t2))
+            return newVectices[t2];
+        if (newVectices.ContainsKey(t1))
+            return newVectices[t1];
+
+        newVectices.Add(t1, newIndex);
+
+        vertices.Add((vertices[i1] + vertices[i2]) * 0.5f);
+        if (normals.Count > 0)
+            normals.Add((normals[i1] + normals[i2]).normalized);
+        if (colors.Count > 0)
+            colors.Add((colors[i1] + colors[i2]) * 0.5f);
+        if (uv.Count > 0)
+            uv.Add((uv[i1] + uv[i2]) * 0.5f);
+        if (uv1.Count > 0)
+            uv1.Add((uv1[i1] + uv1[i2]) * 0.5f);
+        if (uv2.Count > 0)
+            uv2.Add((uv2[i1] + uv2[i2]) * 0.5f);
+
+        return newIndex;
+    }
+
+    public static int[] Subdivide(int i1, int i2, int i3)
+    {
+        int[] t_indices = new int[12];
+
+        int a = GetNewVertex4(i1, i2);
+        int b = GetNewVertex4(i2, i3);
+        int c = GetNewVertex4(i3, i1);
+        t_indices[0] = i1;
+        t_indices[1] = a;
+        t_indices[2] = c;
+
+        t_indices[3] = i2;
+        t_indices[4] = b;
+        t_indices[5] = a;
+
+        t_indices[6] = i3;
+        t_indices[7] = c;
+        t_indices[8] = b;
+
+        t_indices[9] = a;
+        t_indices[10] = b;
+        t_indices[11] = c;
+
+        return t_indices;
+    }
+
+    public static void Subdivide(Mesh mesh, int[] levels)
+    {
+        newVectices = new Dictionary<uint, int>();
+
+        InitArrays(mesh);
+
+        int[] triangles = mesh.triangles;
+        int count = triangles.Length / 3;
+
+        int i1, i2, i3;
+
+        for (int i = 0; i < count; i++)
+        {
+            i1 = triangles[i * 3];
+            i2 = triangles[i * 3 + 1];
+            i3 = triangles[i * 3 + 2];
+
+            int suvdivisionLevel = levels[i];
+
+            if(suvdivisionLevel == 1)
+            {
+                var temp_indices_1 = Subdivide(i1, i2, i3);
+
+                indices.AddRange(temp_indices_1);
+                if (suvdivisionLevel == 1)
+                    indices.AddRange(temp_indices_1);
+
+                for (int l2 = 0; suvdivisionLevel >= 2 && l2 < temp_indices_1.Length / 3; l2++)
+                {
+                    i1 = temp_indices_1[l2 * 3];
+                    i2 = temp_indices_1[l2 * 3 + 1];
+                    i3 = temp_indices_1[l2 * 3 + 2];
+
+                    var temp_indices_2 = Subdivide(i1, i2, i3);
+
+                    if (suvdivisionLevel == 2)
+                        indices.AddRange(temp_indices_2);
+
+                    for (int l3 = 0; suvdivisionLevel >= 3 && l3 < temp_indices_2.Length / 3; l3++)
+                    {
+                        i1 = temp_indices_2[l3 * 3];
+                        i2 = temp_indices_2[l3 * 3 + 1];
+                        i3 = temp_indices_2[l3 * 3 + 2];
+
+                        var temp_indices_3 = Subdivide(i1, i2, i3);
+
+                        if (suvdivisionLevel == 3)
+                            indices.AddRange(temp_indices_3);
+
+                        for (int l4 = 0; suvdivisionLevel >= 3 && l4 < temp_indices_3.Length / 3; l4++)
+                        {
+                            i1 = temp_indices_3[l4 * 3];
+                            i2 = temp_indices_3[l4 * 3 + 1];
+                            i3 = temp_indices_3[l4 * 3 + 2];
+
+                            var temp_indices_4 = Subdivide(i1, i2, i3);
+
+                            if (suvdivisionLevel == 4)
+                                indices.AddRange(temp_indices_4);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                indices.Add(i1);
+                indices.Add(i2);
+                indices.Add(i3);
+            }
+
+        }
+
+        //Debug.Log("mesh.vertices : " + vertices.Count);
+        //Debug.Log("mesh.triangles : " + indices.Count);
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = indices.ToArray();
+        if (normals.Count > 0)
+            mesh.normals = normals.ToArray();
+        if (colors.Count > 0)
+            mesh.colors = colors.ToArray();
+        if (uv.Count > 0)
+            mesh.uv = uv.ToArray();
+        if (uv1.Count > 0)
+            mesh.uv2 = uv1.ToArray();
+        if (uv2.Count > 0)
+            mesh.uv2 = uv2.ToArray();
+
+    }
+
+
+    #endregion Subdivide
+
+    public static Mesh DuplicateMesh(Mesh mesh)
+    {
+        return (Mesh)UnityEngine.Object.Instantiate(mesh);
+    }
+}
---
+++ Assets/Scripts/Sampler.cs
@@ -0,0 +1,274 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Sampler : MonoBehaviour
+{
+    MeshFilter mf;
+
+    [SerializeField]
+    Texture2D densityTex;
+
+    float texelSize;
+
+    int[] subdivisionLevels;
+
+    TriangleData[] triangleDatas;
+
+    int triangle_totalCount;
+
+    [SerializeField]
+    int samplingCount;
+
+    Vector3[] points;
+
+    // Use this for initialization
+    void Start()
+    {
+        mf = this.GetComponent<MeshFilter>();
+
+        triangle_totalCount = mf.mesh.triangles.Length / 3;
+
+        var total_triangle_size = CalcuteTriangleTotalSize();
+
+        triangleDatas = new TriangleData[triangle_totalCount];
+
+        for (int i = 0; i < triangle_totalCount; i++)
+        {
+            var t0 = mf.mesh.triangles[i * 3];
+            var t1 = mf.mesh.triangles[i * 3 + 1];
+            var t2 = mf.mesh.triangles[i * 3 + 2];
+
+            var v1 = mf.mesh.vertices[t0];
+            var v2 = mf.mesh.vertices[t1];
+            var v3 = mf.mesh.vertices[t2];
+
+            var uv1 = mf.mesh.uv[t0];
+            var uv2 = mf.mesh.uv[t1];
+            var uv3 = mf.mesh.uv[t2];
+
+            float size = GetTriangleSize(v1, v2, v3);
+
+            TriangleData td = new TriangleData();
+
+            Vector2 centerPos = GetBarycentric(uv1, uv2, uv3);
+
+            float density = 1;
+            if (densityTex != null)
+            {
+                Color c = densityTex.GetPixel((int)(centerPos.x * densityTex.width), (int)(centerPos.y * densityTex.height));
+                density = c.r;
+            }
+
+            float pdf = (density * size) / total_triangle_size;
+            td.index = i;
+            td.pdf = pdf;
+
+            for (int j = 0; j < i; j++)
+            {
+                td.cdf += triangleDatas[j].pdf;
+            }
+
+            td.cdf += pdf;
+
+            triangleDatas[i] = td;
+        }
+
+        DrawRandomPoints();
+
+        #region mesh subdivide
+        //texelSize = densityTex.texelSize.x * densityTex.texelSize.y;
+
+        //subdivisionLevels = new int[mf.mesh.triangles.Length / 3];
+
+        //for (int i = 0; i < mf.mesh.triangles.Length; i+=3)
+        //{
+        //    var t0 = mf.mesh.triangles[i];
+        //    var t1 = mf.mesh.triangles[i + 1];
+        //    var t2 = mf.mesh.triangles[i + 2];
+
+        //    var v1 = mf.mesh.vertices[t0];
+        //    var v2 = mf.mesh.vertices[t1];
+        //    var v3 = mf.mesh.vertices[t2];
+
+        //    Vector3 v = Vector3.Cross(v1 - v2, v1 - v3);
+        //    float size = v.magnitude * 0.5f;
+
+        //    int subdivisionLevel = 0;
+
+        //    while (!checkSize(size))
+        //    {
+        //        ++subdivisionLevel;
+        //        size /= Mathf.Pow(4.0f, subdivisionLevel);
+        //    }
+
+        //    subdivisionLevels[i / 3] = subdivisionLevel;
+
+        //    Debug.Log("level : " + subdivisionLevel);
+        //}
+
+        //Mesh mesh = mf.mesh;
+        //MeshHelper.Subdivide(mesh, subdivisionLevels);   // divides a single quad into 6x6 quads
+        //mf.mesh = mesh;
+
+        #endregion
+    }
+
+    private Vector2 GetBarycentric(Vector2 uv1, Vector2 uv2, Vector2 uv3)
+    {
+        Vector2 bary;
+        bary = (uv1 + uv2 + uv3) / 3.0f;
+
+        return bary;
+    }
+    private void DrawRandomPoints()
+    {
+        points = new Vector3[samplingCount];
+        
+        for (int i = 0; i < samplingCount; i++)
+        {
+            var triangle_index = Bisection(triangleDatas, Random.value);
+
+            if (triangle_index.HasValue)
+            {
+                var rnd1 = Random.value;
+                var rnd2 = Random.value;
+
+                var u = 1 - Mathf.Sqrt(rnd1);
+                var v = rnd2 * Mathf.Sqrt(rnd1);
+
+                var pos = UvToWorldPosition(new Vector2(u, v), triangle_index.Value);
+                points[i] = pos;
+            }
+        }
+    }
+
+    Vector3 UvToWorldPosition(Vector2 uv, int index)
+    {
+        var t1 = mf.mesh.triangles[index * 3];
+        var t2 = mf.mesh.triangles[index * 3 + 1];
+        var t3 = mf.mesh.triangles[index * 3 + 2];
+
+        var u = uv.x;
+        var v = uv.y;
+
+        float aa = u;
+        float bb = v;
+        float cc = 1 - u - v;
+
+        Vector3 p3D = aa * mf.mesh.vertices[t1] + bb * mf.mesh.vertices[t2] + cc * mf.mesh.vertices[t3];
+
+        return transform.TransformPoint(p3D);
+    }
+
+    public int? Bisection(TriangleData[] array, float target)
+    {
+        if (array == null || array.Length == 0)
+            return null;
+
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            float d = (right - left) / 2.0f;
+            if (d <= 0.5f)
+                return right;
+
+            int middle = left + (right - left) / 2;
+
+            if (array[middle].cdf > target)
+            {
+                right = middle;
+            }
+            else if (array[middle].cdf < target)
+            {
+                left = middle;
+            }
+            else
+            {
+                return middle;
+            }
+        }
+
+        Debug.Log("Seek Fail!!");
+        return null;
+    }
+
+    public struct TriangleData
+    {
+        public int index;
+        public float pdf;
+        public float cdf;
+    }
+
+    bool checkSize(float triangle_size)
+    {
+        if (triangle_size > texelSize)
+            return false;
+
+        return true;
+    }
+
+    float CalcuteTriangleTotalSize()
+    {
+        float total_size = 0;
+
+        for (int i = 0; i < triangle_totalCount; i++)
+        {
+            var t1 = mf.mesh.triangles[i * 3];
+            var t2 = mf.mesh.triangles[i * 3 + 1];
+            var t3 = mf.mesh.triangles[i * 3 + 2];
+
+            var v1 = mf.mesh.vertices[t1];
+            var v2 = mf.mesh.vertices[t2];
+            var v3 = mf.mesh.vertices[t3];
+
+            var size = GetTriangleSize(v1, v2, v3);
+
+            var uv1 = mf.mesh.uv[t1];
+            var uv2 = mf.mesh.uv[t2];
+            var uv3 = mf.mesh.uv[t3];
+
+            Vector2 barycentricPos = GetBarycentric(uv1, uv2, uv3);
+
+            float density = 1;
+            if (densityTex != null)
+            {
+                Color c = densityTex.GetPixel((int)(barycentricPos.x * densityTex.width), (int)(barycentricPos.y * densityTex.height));
+                density = c.r;
+            }
+
+            total_size += (density * size);
+        }
+
+        return total_size;
+    }
+
+    private float GetTriangleSize(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        Vector3 v = Vector3.Cross(v1 - v2, v1 - v3);
+        float size = v.magnitude * 0.5f;
+        return Mathf.Abs(size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+            DrawRandomPoints();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(points != null)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                var pos = points[i];
+                Gizmos.DrawSphere(pos, 0.01f);
+            }
+        }
+    }
+}
